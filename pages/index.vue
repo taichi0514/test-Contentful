420c8aaa8e64ab2a01dfc65a74fec38e8742d8b3
taichimurai <murai@tam-bourine.co.jp>
2019-04-01 18:29:25 +0900

    add npm install contentful

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,5 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        test-Contentful
-      </h1>
-      <h2 class="subtitle">
-        My super-duper Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
-  </section>
+  <section class="container"></section>
 </template>
 
 <script>
